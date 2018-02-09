a1266507a2ae241c81b21f2b533acf49c92d776c
LordHorst <36306279+LordHorst@users.noreply.github.com>
2018-02-09 18:02:11 +0100

    Starting out

---
+++ TestingConsoleStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestingConsoleStuff
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
